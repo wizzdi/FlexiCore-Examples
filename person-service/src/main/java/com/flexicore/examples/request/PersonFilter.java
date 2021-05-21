a1dda8fd4b9cf26d637e974dd4432d530913dd96
Asaf Ben Natan <asaf@wizzdi.com>
2021-05-21 18:27:55 +0300

    added as serializable

--- person-service/src/main/java/com/flexicore/examples/request/PersonFilter.java
+++ person-service/src/main/java/com/flexicore/examples/request/PersonFilter.java
@@ -2,8 +2,7 @@ package com.flexicore.examples.request;
 
 import com.wizzdi.flexicore.security.request.PaginationFilter;
 
-import java.io.Serializable;
 
-public class PersonFilter extends PaginationFilter implements Serializable {
+public class PersonFilter extends PaginationFilter {
 
 }
